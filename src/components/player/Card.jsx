1c74bd5c119b9de1e3b40797edfdb04d2ff07edd
lhqtapcode <student230228@ptnk.edu.vn>
2025-04-07 21:43:26 +0700

    Update Card.jsx

--- src/components/player/Card.jsx
+++ src/components/player/Card.jsx
@@ -1,17 +1,22 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import "../../styles/card.css"
-import { Player } from '../../types/roles';
-
-// import card from "../../assets/player/Anh Hàng Xóm.png";
+import getPlayerImage from '../../assets/playerImages';
 
 function Card(props) {
-    const card = require(`../../assets/player/${props.player.role}.png`);
+    // Lấy đường dẫn hình ảnh từ map dựa vào vai trò của người chơi
+    const cardImage = getPlayerImage(props.player.role);
 
-    return (<div className='card'>
-        <div className='background-card'>
-            <img src={card}></img>
+    return (
+        <div className='card'>
+            <div className='background-card'>
+                <img src={cardImage} alt={props.player.role} />
+            </div>
+            <div className='player-info'>
+                <h3>{props.player.name}</h3>
+                <p>{props.player.role}</p>
+            </div>
         </div>
-    </div>);
+    );
 }
 
 export default Card;
